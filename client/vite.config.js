e1d96de15f02fe17e7a6352c3a86e74af2343868
woldpt <woldpt@set84.com>
2026-03-26 09:38:42 +0000

    login issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  server: {
+    proxy: {
+      "/auth": "http://localhost:3000",
+      "/saves": "http://localhost:3000",
+    },
+  },
+});
